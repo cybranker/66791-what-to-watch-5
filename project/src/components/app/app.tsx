35b53d79f3937e41eb35e62059d415f7dc962c58
Cybranker <cybranker@yandex.ru>
2022-06-21 17:32:52 +0300

    Add React Components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+
+type AppScreenProps = {
+  currentFilm: {
+    NAME: string;
+    GENRE: string;
+    RELEASE_DATE: number;
+  }
 }
+
+function App({ currentFilm }: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen currentFilm={currentFilm} />
+  );
+}
+
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+
+const Film = {
+  NAME: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE_DATE: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App currentFilm={Film} />
   </React.StrictMode>,
   document.getElementById('root'));
